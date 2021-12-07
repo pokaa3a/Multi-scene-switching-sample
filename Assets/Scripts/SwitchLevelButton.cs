bd350affb9178a4effe83faa9a01ef89e7cb2613
pokaa3a <pkhuang@umich.edu>
2021-12-07 00:29:51 -0800

    rename SwitchLevelButton

--- Assets/Scripts/SwitchLevelButton.cs
+++ Assets/Scripts/SwitchLevelButton.cs
@@ -5,12 +5,12 @@ using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class SwitchSceneButton : MonoBehaviour, IPointerClickHandler
+public class SwitchLevelButton : MonoBehaviour, IPointerClickHandler
 {
     public void OnPointerClick(PointerEventData data)
     {
         int nextLevel = LevelManager.curLevel == 1 ? 2 : 1;
-
+        Debug.Log($"Switch to level {nextLevel}");
         LevelManager.GoToLevel(nextLevel);
     }
 }
